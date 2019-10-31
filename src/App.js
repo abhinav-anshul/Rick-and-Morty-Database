fcd8a06275a9bb5be519e2ba9293b279cf60505d
abhinav anshul <abhinavanshul4u@gmail.com>
2019-10-31 17:10:22 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//const BASE_URL = "https://rickandmortyapi.com/api/";
+
+class App extends React.Component {
+  getData = async e => {
+    e.preventDefault();
+    const url = await axios("https://rickandmortyapi.com/api/character");
+    //const newData = url.data.results[2].name;
+
+    const newData = url.data.results.forEach(function(element) {
+      return console.log(element.name);
+    });
+  };
+
+  state = {
+    url: "https://rickandmortyapi.com/api/character",
+    character: null
+  };
+
+  async componentDidMount() {
+    const res = await axios.get(this.state.url);
+    this.setState({
+      character: res.data.results.forEach(function(element) {
+        return element.name;
+      })
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        {/* <Character getData={this.getData} /> */}
+        <h1>{this.state.character}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
